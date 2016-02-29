598daec2221852e4dd705de98f80d7ce4cd90e31
ashkaren <gvevtodiev@gmail.com>
2016-02-29 14:54:40 -0500

    I1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221221514) do
+ActiveRecord::Schema.define(version: 20160229193335) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at"
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 20160221221514) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.decimal  "popularity",                                      default: 0.0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
